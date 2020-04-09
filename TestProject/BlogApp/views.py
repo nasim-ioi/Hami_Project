94bfdbb7afc7bb53575383a8b35d0d16ed71373b
nasim <rezaie.nasim.imo@gmail.com>
2020-04-10 03:49:21 +0430

    create blog viewset and singup view

---
+++ TestProject/BlogApp/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework import status
+
+from .models import Blog
+from .serializers import UserSerializer, BlogSerializer
+
+
+class Signup(APIView):
+
+    serializer_class = UserSerializer
+
+    def post(self, request):
+        serializer = UserSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'status': 'you signed up successfully'})
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+class BlogViewSet(ModelViewSet):
+    """
+    create :  
+        creating a Blog object by an authenticated user
+    
+    list :
+        displaying all the Blog objects to an authenticated user
+    
+    retreive :
+        displaying a specific Blog object to an authenticated user
+    
+    update :
+        updating a specific Blog object information by an authenticated user
+    
+    partial_update :
+        partial updating a specific Blog object information by an authenticated user
+    
+    destroy :
+        deleting a specific Blog object by an authenticated user
+
+    """
+
+    # only athenticated users(which have logged in) can create, read, update or delete their blog
+    permission_classes = [IsAuthenticated]
+
+    queryset = Blog.objects.all()
+
+    serializer_class = BlogSerializer
